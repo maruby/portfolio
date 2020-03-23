e62868ff435ff12069757b6a87f88a0091cd8a2f
maruby <49580815+maruby@users.noreply.github.com>
2020-03-24 00:05:14 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -22,9 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
